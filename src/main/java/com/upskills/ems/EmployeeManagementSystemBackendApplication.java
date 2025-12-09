be288dca47c327933e08f0169c3e083494bedc1b
Mohankumar-parise <mohankumar.parise523@gmail.com>
2025-12-09 18:39:12 +0530

    my demo uploaded

---
+++ src/main/java/com/upskills/ems/EmployeeManagementSystemBackendApplication.java
@@ -0,0 +1,14 @@
+package com.upskills.ems;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeeManagementSystemBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmployeeManagementSystemBackendApplication.class, args);
+		System.out.println("Employee Management System Backend is running...");
+	}
+
+}
